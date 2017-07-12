5403cc8d461272b31cb4ab7253d3bb81efdac19f
Keitaro0408 <kei.kotani77@gmail.com>
2017-07-12 21:44:31 +0900

    作業内容
    - スマートポインタを追加(実装中)

---
+++ Library/Library/Library/Pointer/UniquePtr.h
@@ -0,0 +1,74 @@
+﻿/**
+* @file   UniquePtr.h
+* @brief  UniquePtrクラスのヘッダファイル
+* @author kotani
+*/
+#ifndef UNIQUE_PTR_H
+#define UNIQUE_PTR_H
+
+#define EMPTY_POINTER(ptr) \
+		!ptr
+
+
+namespace Lib
+{
+	/**
+	 * 所有権が1つしか持てない様に振る舞うスマートポインタ
+	 * カスタムデリータも実装予定
+	 */
+	template<typename Type>
+	class UniquePtr
+	{
+	public:
+		UniquePtr(Type* _type);
+		UniquePtr();
+
+		~UniquePtr();
+
+		/**
+		 * ポインタを取得する
+		 */
+		const Type* GetPtr();
+
+		/**
+		 * メモリを解放する
+		 */
+		void Reset();
+
+		/**
+		 * メモリを解放した後に引数のポインタを扱う
+		 * @param[in] _type 新しく管理するポインタ
+		 */
+		void Reset(Type* _type);
+
+		/**
+		 * リソースの管理を放棄する
+		 * @return 管理を破棄したポインタ
+		 */
+		Type* Release();
+
+		/**
+		 * 直接代入の禁止
+		 */
+		UniquePtr& operator=(const UniquePtr&) = delete;
+
+		/**
+		 * メンバにアクセスする
+		 */
+		Type* operator->() const;
+
+		/**
+		 * 中身が空かチェック
+		 */
+		operator bool() const;
+
+	private:
+		Type* m_Instance;
+
+	};
+
+
+#include "UniquePtr_private.h"
+}
+
+#endif
---
+++ Library/Library/Library/Pointer/UniquePtr_private.h
@@ -0,0 +1,71 @@
+﻿#ifndef UNIQUE_PTR_PRIVATE_H
+#define UNIQUE_PTR_PRIVATE_H
+
+template<typename Type>
+UniquePtr<Type>::UniquePtr(Type* _type)
+{
+	m_Instance = _type;
+}
+
+template<typename Type>
+UniquePtr<Type>::UniquePtr()
+{
+	m_Instance = nullptr;
+}
+
+template<typename Type>
+UniquePtr<Type>::~UniquePtr()
+{
+	delete m_Instance;
+	m_Instance = nullptr;
+}
+
+template<typename Type>
+const Type* UniquePtr<Type>::GetPtr()
+{
+	return m_Instance;
+}
+
+template<typename Type>
+void UniquePtr<Type>::Reset()
+{
+	delete m_Instance;
+	m_Instance = nullptr;
+}
+
+template<typename Type>
+void UniquePtr<Type>::Reset(Type* _type)
+{
+	delete m_Instance;
+	m_Instance = nullptr;
+	m_Instance = _type;
+}
+
+template<typename Type>
+Type* UniquePtr<Type>::Release()
+{
+	Type* returnVal = m_Instance;
+	m_Instance = nullptr;
+
+	return returnVal;
+}
+
+template<typename Type>
+Type* UniquePtr<Type>::operator->() const
+{
+	return m_Instance;
+}
+
+template<typename Type>
+UniquePtr<Type>::operator bool() const
+{
+	if (m_Instance != nullptr)
+	{
+		return true;
+	}
+
+	return false;
+}
+
+
+#endif
